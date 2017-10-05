13c300aa2b12049bbfa5384c5fae4b5e709a6610
Wesley.Yuan <torresdyl@gmail.com>
2017-10-05 16:27:15 +0200

    final day

---
+++ src/com/WindThunderStudio/Interview/Heratige/Superman.java
@@ -0,0 +1,10 @@
+package com.WindThunderStudio.Interview.Heratige;
+
+public class Superman implements Fly {
+
+    @Override
+    public void fly() {
+        System.out.println("Superman can fly!");
+    }
+
+}
