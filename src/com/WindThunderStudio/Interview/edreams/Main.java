85fb0672697572fb8b20ec009f27fc01eacdab9d
Yangliang Ding <yangliang.ding@acuntia.dom>
2018-05-04 15:59:58 +0200

    sum-distance

---
+++ src/com/WindThunderStudio/Interview/edreams/Main.java
@@ -0,0 +1,14 @@
+package com.WindThunderStudio.Interview.edreams;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Test1 test1 = new Test1();
+//        int[] A = new int[] {-8, 4, 0, 5, -3, 6};
+//        int[] A = new int[] {1, 3, -3};
+//        int[] A = new int[] {-8, 6, 0, 5, -3, 4};
+        int[] A = new int[] {-5, 6, 8, 0, 1, -9, 10, 11, -20};
+        System.out.println(test1.solution(A));
+    }
+
+}
---
+++ src/com/WindThunderStudio/Interview/geekforgeek/string/SavingIronmanAmazon.java
@@ -0,0 +1,72 @@
+package com.WindThunderStudio.Interview.geekforgeek.string;
+/**
+ * https://practice.geeksforgeeks.org/problems/save-ironman/
+ * Jarvis is weak in computing palindromes for Alphanumeric characters.
+ * While Ironman is busy fighting Thanos, he needs to activate sonic punch but Jarvis is stuck in computing palindromes.
+ * You are given a string containing the alphanumeric character. Find whether the string is palindrome or not.
+ * If you are unable to solve it then it may result in the death of Iron Man.
+ * 
+ * Input:
+ * The first line of the input contains t, the number of test cases. Each line of the test case contains string 'S'.
+ * 
+ * Output:
+ * Each new line of the output contains "YES" if the string is palindrome and "NO" if the string is not a palindrome.
+ * 
+ * Constraints:
+ * 1<=t<=100
+ * 1<=|S|<=100000
+ * Note: Consider alphabets and numbers only for palindrome check. Ignore symbols and whitespaces.
+ * 
+ * Example:
+ * Input:
+ * 2
+ * I am :IronnorI Ma, i
+ * Ab?/Ba
+ * 
+ * Output:
+ * YES
+ * YES
+ * @author yangliang.ding
+ *
+ */
+public class SavingIronmanAmazon {
+    public static void main(String[] args) {
+        SavingIronmanAmazon savingIronmanAmazon = new SavingIronmanAmazon();
+        String s1 = "I am :IronnorI Ma, i";
+        String s2 = "Ab?/Ba";
+        System.out.println(savingIronmanAmazon.saveIronman(s1));
+        System.out.println(savingIronmanAmazon.saveIronman(s2));
+    }
+    public String saveIronman(String s) {
+        //remove all non-alphanumeric characters in a string
+        // - if all letters are latin letters:
+        String noSymbol = s.replaceAll("[^a-zA-Z0-9]", "");
+        // - if we must consider Unicode letters: \\p{L} is the definition of an Unicode letter, according to:
+        // https://stackoverflow.com/questions/43263680/removing-all-characters-but-letters-in-a-string#answer-43263912
+        // and:
+        // http://www.regular-expressions.info/unicode.html#prop.
+        // \\P{L}: not letter \\p{L}: letter
+        String noSymbolUnicode = s.replaceAll("[^\\P{L}0-9]", "");
+        // the most complete case:\P{M}\p{M}*+, to match different encoding of letters with diacritics. (one code point or two)
+        String noSymbolUnicodeComplete = s.replaceAll("[^\\P{M}\\p{M}*0-9]", "");
+        
+        //delete whitespace
+        noSymbolUnicodeComplete = noSymbolUnicodeComplete.replaceAll(" ", "");
+        
+        System.out.println("Preprocessed string: " + noSymbolUnicodeComplete);
+        if (noSymbolUnicodeComplete.length() == 1) {
+            return "YES";
+        } else {
+            int length = noSymbolUnicodeComplete.length();
+            for (int i=0; i<(int)length/2; i++) { //no matter is even or odd
+                if (noSymbolUnicodeComplete.charAt(i) != noSymbolUnicodeComplete.charAt(length-1-i)) {
+                    return "NO";
+                } else {
+                    continue;
+                }
+                
+            }
+            return "YES";
+        }
+    }
+}
