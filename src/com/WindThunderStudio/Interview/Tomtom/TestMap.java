bf8a98926df5c19b190eb1e12b66684cbcb34080
WesternGun <torresdyl@gmail.com>
2018-05-07 21:12:23 +0200

    merge; java file

--- src/com/WindThunderStudio/Interview/Tomtom/TestMap.java
+++ src/com/WindThunderStudio/Interview/Tomtom/TestMap.java
@@ -6,12 +6,18 @@ import java.util.Map;
 public class TestMap {
     public static void main(String[] args) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(1, 0);
-        map.put(2, 0);
-        map.put(3, 0);
+        map.put(1, 555);
+        map.put(2, 666);
+        map.put(3, 777);
         System.out.println(map.size());
+        for (Integer i: map.keySet()) {
+            System.out.println(map.get(i));
+        }
         
         Integer r = (Integer)map.get(1);
         System.out.println(map.size());
+        for (Integer i: map.keySet()) {
+            System.out.println(map.get(i));
+        }
     }
 }
